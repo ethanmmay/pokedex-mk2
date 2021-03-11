adb65b0b379183d723b8a9d00363697a5e3eab46
Ashley Herrera <ashleyherrera131@gmail.com>
2021-03-11 14:56:45 -0700

    card template

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="col-6">
+    <div class="card pokemon">
+      <img class="card-img-top" :src="" alt="pokemon">
+      <div class="card-body">
+          <h4 class="card-title">Name: {{pokemon.name}}</h4>
+          <p calss="card-text">Type: {{}}</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
